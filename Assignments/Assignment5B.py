4839b0cb41443dcafe70dd05767592522ec37390
ethereralone-rgb <ethereralone@gmail.com>
2026-08-11 13:24:45 -0400

    Working on creating grids

--- Assignments/Assignment5B.py
+++ Assignments/Assignment5B.py
@@ -1,5 +1,25 @@
+import random
+
+
 def main():
-    pass
+    
+    rows = int(input("Enter the rows: "))
+    col = int(input("Enter the col: "))
+    grid = []
+    available_seats = 0 
+    for r in range(rows):
+        row = []
+        for c in range(col):
+            chance = random.random()
+            if chance >= 0.7:
+                row.append("R")
+            else:
+                row.append("A")
+                available_seats += 1 
+        grid.append(row)
+    for row in grid:
+        print(row)
+    print(available_seats)
 
 if __name__ == "__main__":
     main()
