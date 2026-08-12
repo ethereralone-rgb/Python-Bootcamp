3e5df68eae6fb30d6df872e7a5c6c97519178a05
ethereralone-rgb <ethereralone@gmail.com>
2026-08-12 00:39:50 -0400

    New drills

--- Assignments/Assignment5B.py
+++ Assignments/Assignment5B.py
@@ -1,10 +1,15 @@
 import random
 
+def print_chart(grid):
+    for row in grid:
+        print(" ".join(row))
+    
 
 def main():
     
-    rows = int(input("Enter the rows: "))
-    col = int(input("Enter the col: "))
+    rows = int(input("Enter the number of rows: "))
+    col = int(input("Enter the number of columns: "))
+    
     grid = []
     available_seats = 0 
     for r in range(rows):
@@ -17,9 +22,30 @@ def main():
                 row.append("A")
                 available_seats += 1 
         grid.append(row)
-    for row in grid:
-        print(row)
-    print(available_seats)
+    print_chart(grid)
+    print(f"Number of available seats: {available_seats}")
+    print()
+   
+    while available_seats > 0:
+        chosen_row = int(input(f"Enter the row number (0 to {rows - 1}): "))
+        chosen_cols = int(input(f"Enter the column number (0 to {col - 1}): "))
+        seat = (chosen_row, chosen_cols)
+
+        if grid[chosen_row][chosen_cols] == "A":
+            grid[chosen_row][chosen_cols] = "B"
+            available_seats -= 1
+            print("Seat booked succesfully!")
+        elif grid[chosen_row][chosen_cols] == "R":
+            print("This seat is already reserved")
+        elif grid[chosen_row][chosen_cols] == "B":
+            print("This seat has already been booked.")
+        print_chart(grid)
+        print()
+
+        
+            
+
+
 
 if __name__ == "__main__":
     main()
