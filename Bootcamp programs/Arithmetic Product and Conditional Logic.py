15287f481ab47163132dfcc70f840909d6600d75
ethereralone-rgb <ethereralone@gmail.com>
2026-08-13 17:24:02 -0400

    renaming files

---
+++ Bootcamp programs/Arithmetic Product and Conditional Logic.py
@@ -0,0 +1,16 @@
+def product(x,y):
+    product = x * y 
+    if product < 1000:
+        return product
+    else:
+        sum = x + y
+        return sum
+
+
+def main():
+    x = int(input("Number1: "))
+    y = int(input("Number2: "))
+    print(product(x,y))
+
+if __name__ == "__main__":
+    main()
